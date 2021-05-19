ef3dbe48b8cd58b90428ecb77a4320577632509d
Carlos-Reis <carlos._reis@hotmail.com>
2021-05-19 04:45:29 +0100

    consApp para calculos simples

---
+++ ConsoleApp1/Calculos.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CalculosConsole
+{
+    public class Calculos
+    {
+        public static void CalculaMedia()
+        {
+            List<double> itens = new List<double>();
+            int totalItens = 0;
+            Console.Write("Informe a quantidade de itens: ");
+            try
+            {
+                totalItens = Convert.ToInt32(Console.ReadLine());
+            }
+            catch 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("É necessário informar um número");
+                Console.ResetColor();
+                throw;
+            }
+
+            int i = 0;
+            while (i < totalItens)
+            {
+                i++;
+                try
+                {
+                    Console.Write("Informe o " + i + "º item : ");
+                    itens.Add(Convert.ToDouble(Console.ReadLine()));
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write("É necessário informar um número");
+                    Console.ResetColor();
+                    throw;
+                }
+            }
+
+            var media = itens.Sum() / totalItens;
+            Console.WriteLine("A média é " + media);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CalculosConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool restart = true;
+            int operacao = 0;
+            do
+            {
+                #region Cabeçalho Console
+                Console.Clear();
+                Console.WriteLine("Programa que faz cálculos");
+                Console.WriteLine("Indique a operação desejada:");
+                Console.WriteLine("1. Calcular Média");
+                #endregion
+
+                #region Operações
+                try
+                {
+                    try
+                    {
+                        operacao = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("Por favor, digite um número para selecionar a operação");
+                        throw;
+                    }
+
+                    switch (operacao)
+                    {
+                        case 1:
+                            Calculos.CalculaMedia();
+                            break;
+                        default:
+                            Calculos.CalculaMedia();
+                            break;
+                    }
+                }
+                catch
+                {
+                    Console.ResetColor();
+                    Console.WriteLine("\r\n");
+                }
+                #endregion
+
+                Console.WriteLine("Aperte R para recomeçar ou qualquer outra tecla para encerrar");
+
+                var end = Console.ReadKey();
+                if (end.Key != ConsoleKey.R)
+                {
+                    restart = false;
+                }
+            }
+            while (restart);
+        }
+    }
+}
